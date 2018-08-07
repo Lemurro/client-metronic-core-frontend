fa6b6fa0cb3cd3adae6c814fb37714493fb9017f
DimNS <atomcms@ya.ru>
2018-08-07 15:00:25 +0500

    Инициализация переменных перенесена в метод init()

--- src/js/400-guide/0-init.js
+++ src/js/400-guide/0-init.js
@@ -1,7 +1,7 @@
 /**
  * Операции со справочниками
  *
- * @version 13.05.2018
+ * @version 07.08.2018
  * @author Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -13,28 +13,24 @@
 var guide = {};
 
 /**
- * Тип справочника
- *
- * @type {string}
- */
-guide.type = '';
-
-/**
- * Объект для хранения шаблонов
- *
- * @type {object}
- */
-guide.templates = {};
-
-/**
  * Инициализация
  *
- * @version 13.05.2018
+ * @version 07.08.2018
  * @author Дмитрий Щербаков <atomcms@ya.ru>
  */
 guide.init = function () {
+    /**
+     * Тип справочника
+     *
+     * @type {string}
+     */
     guide.type = $('#js-page').attr('data-type');
 
+    /**
+     * Объект для хранения шаблонов
+     *
+     * @type {object}
+     */
     guide.templates = {
         item: Template7.compile($('#js-tpl-guide__item').html())
     };
