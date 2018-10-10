366b4195d05e5647b64f126be07c9727bee80e0e
DimNS <atomcms@ya.ru>
2018-10-10 14:36:07 +0500

    Добавлена возможность при возврате ошибки 403 не делать редирект

--- src/js/0-init/200-showErrors.js
+++ src/js/0-init/200-showErrors.js
@@ -1,40 +1,62 @@
 /**
  * Отображение ошибок
  *
- * @param errors array Массив ошибок
+ * @param {array} errors Массив ошибок
  *
- * @version 08.08.2018
+ * @version 10.10.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 bootstrap.showErrors = function (errors) {
     if (errors.length === 1 && errors[0].status === '401 Unauthorized') {
         bootstrap.authScreen('show');
     } else if (errors.length === 1 && errors[0].status === '403 Forbidden') {
-        location.assign(location.origin + '/403');
+        var redirect = true;
+
+        if (errors[0].hasOwnProperty('meta') && errors[0].meta.hasOwnProperty('redirect')) {
+            redirect = errors[0].meta.redirect;
+        }
+
+        if (redirect) {
+            location.assign(location.origin + '/403');
+        } else {
+            bootstrap._showError(errors[0].code, errors[0].title);
+        }
     } else {
         for (var i in errors) {
-            var item  = errors[i];
-            var title = 'Неизвестная ошибка';
-            var code  = 'error';
-
-            switch (item.code) {
-                case 'danger':
-                    code  = 'error';
-                    title = 'Критическая ошибка';
-                    break;
-
-                case 'warning':
-                    code  = 'warning';
-                    title = 'Внимание!';
-                    break;
-
-                case 'info':
-                    code  = 'info';
-                    title = 'Информация';
-                    break;
-            }
-
-            swal(title, item.title, code);
+            bootstrap._showError(errors[i].code, errors[i].title);
         }
     }
+};
+
+/**
+ * Покажем ошибку
+ *
+ * @param {string} errCode  Код ошибки
+ * @param {string} errTitle Текст ошибки
+ *
+ * @version 10.10.2018
+ * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ */
+bootstrap._showError = function (errCode, errTitle) {
+    var title = 'Неизвестная ошибка';
+    var code  = 'error';
+
+    switch (errCode) {
+        case 'danger':
+            code  = 'error';
+            title = 'Критическая ошибка';
+            break;
+
+        case 'warning':
+            code  = 'warning';
+            title = 'Внимание!';
+            break;
+
+        case 'info':
+            code  = 'info';
+            title = 'Информация';
+            break;
+    }
+
+    swal(title, errTitle, code);
 };
