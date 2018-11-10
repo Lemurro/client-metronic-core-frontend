7fc526a397ead4a271b5a2e8ecfb60d73b0d7954
DimNS <atomcms@ya.ru>
2018-11-10 09:58:58 +0500

    refactor: JS-Библиотеки, используемые ядром вынесены в ядро, больше не нужно их подключать отдельно и не получится что-то неосторожно сломать

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,51 @@ var concat   = require('gulp-concat');    // Склейка файлов
 var rename   = require('gulp-rename');    // Переименование файлов
 var uglify   = require('gulp-uglify');    // Минификация JS
 
-gulp.task('default', ['css', 'js'], function () {
-    gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('src/js/**/*.js', ['js']);
+gulp.task('default', ['build', 'watcher.css', 'watcher.js']);
+gulp.task('build', ['core.css', 'core.js']);
+
+gulp.task('watcher.css', function () {
+    return gulp.watch('src/css/*.css', ['core.css']);
 });
 
-gulp.task('build', ['css', 'js'], function () {
-
+gulp.task('watcher.js', function () {
+    return gulp.watch('src/js/**/*.js', ['core.js']);
 });
 
-gulp.task('css', function () {
-    gulp.src('src/css/*.css')
+gulp.task('core.css', function () {
+    var files = [
+        'node_modules/jquery-lightajax/dist/jquery.lightajax.min.css',
+        'src/css/*.css'
+    ];
+
+    gulp.src(files)
         .pipe(concat('core.css'))
         .pipe(gulp.dest('dist'));
 
-    gulp.src('src/css/*.css')
+    gulp.src(files)
         .pipe(concat('core.css'))
         .pipe(cleanCSS())
         .pipe(rename('core.min.css'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function () {
-    gulp.src('src/js/**/*.js')
+gulp.task('core.js', function () {
+    var files = [
+        'node_modules/bowser/bundled.js',
+        'node_modules/dimns-is-empty-js/dist/isEmpty.min.js',
+        'node_modules/jquery-lightajax/dist/jquery.lightajax.min.js',
+        'node_modules/jquery-tablefilter/dist/jquery.tablefilter.min.js',
+        'node_modules/localforage/dist/localforage.min.js',
+        'node_modules/select2/dist/js/i18n/ru.js',
+        'node_modules/template7/dist/template7.min.js',
+        'src/js/**/*.js'
+    ];
+
+    gulp.src(files)
         .pipe(concat('core.js'))
         .pipe(gulp.dest('dist'));
 
-    gulp.src('src/js/**/*.js')
+    gulp.src(files)
         .pipe(concat('core.js'))
         .pipe(uglify())
         .pipe(rename('core.min.js'))
