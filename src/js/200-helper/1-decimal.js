a17e7d8ace99c1b9cd17610ce82f72324b060270
DimNS <atomcms@ya.ru>
2018-12-07 15:00:18 +0500

    feat: Доработка хелпера lemurro.helper.decimal

--- src/js/200-helper/1-decimal.js
+++ src/js/200-helper/1-decimal.js
@@ -1,13 +1,35 @@
 /**
  * Преобразование строки в дробное число
  *
- * @param {string} str Строка с дробным числом
+ * @param {string|float} value     Строка с дробным числом или число
+ * @param {integer}      precision Точность результата (по умолчанию: 2)
  *
- * @return {string}
+ * @return {float}
  *
- * @version 28.10.2018
+ * @version 07.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
-lemurro.helper.decimal = function (str) {
-    return parseFloat(str.replace(/,/g, '.'));
+lemurro.helper.decimal = function (value, precision) {
+    if (value === undefined) {
+        return 0;
+    }
+
+    if (precision === undefined || typeof precision !== 'number') {
+        precision = 2;
+    }
+
+    if (typeof value === 'number') {
+        return parseFloat(value.toFixed(precision));
+    } else {
+        value = value.replace(/ /g, '');
+        value = value.replace(/,/g, '.');
+
+        value = parseFloat(value);
+
+        if (isNaN(value)) {
+            return 0;
+        }
+
+        return parseFloat(value.toFixed(precision));
+    }
 };
