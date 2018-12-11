8967d8b24be55368cc48fcabfb1c4d321eb50662
DimNS <atomcms@ya.ru>
2018-12-11 11:58:53 +0500

    refactor: Упрощена настройка прав доступа

--- src/js/500-users/100-_setRoles.js
+++ src/js/500-users/100-_setRoles.js
@@ -1,34 +1,32 @@
 /**
  * Нарисуем форму выбора прав доступа
  *
- * @version 30.11.2018
+ * @version 11.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 lemurro.users._setRoles = function () {
     var container = $('#js-user__roles');
     var userRoles = app.config.userRoles;
 
-    for (var pageCode in userRoles.guidePages) {
-        if (userRoles.guidePages.hasOwnProperty(pageCode) && userRoles.list.hasOwnProperty(pageCode)) {
-            var pageName = userRoles.guidePages[pageCode];
-            var list     = userRoles.list[pageCode];
-            var access   = [];
+    for (var keyList in userRoles.list) {
+        var role   = userRoles.list[keyList];
+        var access = [];
 
-            for (var i in list) {
-                var code = list[i];
-                var name = (userRoles.guideAccess.hasOwnProperty(code) ? userRoles.guideAccess[code] : '!unknown');
 
-                access.push({
-                    code: code,
-                    name: name
-                });
-            }
+        for (var keyAccess in role.access) {
+            var code = role.access[keyAccess];
+            var name = (userRoles.guideAccess.hasOwnProperty(code) ? userRoles.guideAccess[code] : '!unknown');
 
-            container.append(lemurro.users._templates.roles({
-                pageName: pageName,
-                pageCode: pageCode,
-                access  : access
-            }));
+            access.push({
+                code: code,
+                name: name
+            });
         }
+
+        container.append(lemurro.users._templates.roles({
+            roleName : role.name,
+            roleTitle: role.title,
+            access   : access
+        }));
     }
 };
