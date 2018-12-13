bfe0871f6729733fbddc915b085c1e053a67052a
DimNS <atomcms@ya.ru>
2018-12-13 14:46:51 +0500

    refactor: Зона UTC принята за стандарт

--- src/js/200-helper/1-localTime.js
+++ src/js/200-helper/1-localTime.js
@@ -4,13 +4,13 @@
  * @param {string} datetime Дата и время в формате "ГГГГ-ММ-ДД ЧЧ:ММ:СС"
  * @param {string} format   Возвращаемый формат (Moment.js)
  *
- * @version 12.12.2018
+ * @version 13.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 lemurro.helper.localTime = function (datetime, format) {
     if (isEmpty(datetime) || isEmpty(format)) {
         return datetime;
     } else {
-        return moment(datetime).local().format(format);
+        return moment.utc(datetime).local().format(format);
     }
 };
