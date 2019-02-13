e1495bfd5d5e33f28495da7e126b902459d6d4bb
Дмитрий Щербаков <atomcms@ya.ru>
2019-02-13 14:41:11 +0500

    feat Переход на Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
-var gulp        = require('gulp');           // Сам Gulp JS
-var cleanCSS    = require('gulp-clean-css'); // Минификация CSS
-var concat      = require('gulp-concat');    // Склейка файлов
-var rename      = require('gulp-rename');    // Переименование файлов
-var uglify      = require('gulp-uglify');    // Минификация JS
-var del         = require('del');            // Удаление файлов
-var runSequence = require('run-sequence');   // Выполнение gulp-задач по очереди
+var gulp     = require('gulp');           // Сам Gulp JS
+var cleanCSS = require('gulp-clean-css'); // Минификация CSS
+var concat   = require('gulp-concat');    // Склейка файлов
+var rename   = require('gulp-rename');    // Переименование файлов
+var uglify   = require('gulp-uglify');    // Минификация JS
+var del      = require('del');            // Удаление файлов
 
 var cssLibs = [
     'node_modules/jquery-lightajax/dist/jquery.lightajax.css'
@@ -38,28 +37,9 @@ var jsLibsMin = [
     'node_modules/template7/dist/template7.min.js'
 ];
 
-// TASKS
-
-gulp.task('default', function () {
-    runSequence('build', ['watcher.css', 'watcher.js']);
-});
-gulp.task('build', function () {
-    runSequence(
-        'core.css',
-        'core.min.css',
-        'concat.css',
-        'concat.min.css',
-        'core.js',
-        'core.min.js',
-        'concat.js',
-        'concat.min.js',
-        'clean'
-    );
-});
-
 // CLEAN
 
-gulp.task('clean', function () {
+function clean() {
     var files = [
         'dist/core.css',
         'dist/core.min.css',
@@ -70,92 +50,87 @@ gulp.task('clean', function () {
     return del(files, {
         force: true
     });
-});
+}
 
 // WATCHER
 
-gulp.task('watcher.css', function () {
-    return gulp.watch('src/css/*.css', function () {
-        runSequence(
-            'core.css',
-            'core.min.css',
-            'concat.css',
-            'concat.min.css',
-            'clean'
-        );
-    });
-});
+function watcherCSS() {
+    return gulp.watch('src/css/*.css', gulp.series(coreCSS, coreMinCSS, concatCSS, concatMinCSS, clean));
+}
 
-gulp.task('watcher.js', function () {
-    return gulp.watch('src/js/**/*.js', function () {
-        runSequence(
-            'core.js',
-            'core.min.js',
-            'concat.js',
-            'concat.min.js',
-            'clean'
-        );
-    });
-});
+function watcherJS() {
+    return gulp.watch('src/js/**/*.js', gulp.series(coreJS, coreMinJS, concatJS, concatMinJS, clean));
+}
 
 // CSS
 
-gulp.task('core.css', function () {
+function coreCSS() {
     return gulp.src('src/css/*.css')
         .pipe(concat('core.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('core.min.css', function () {
+function coreMinCSS() {
     return gulp.src('dist/core.css')
         .pipe(cleanCSS())
         .pipe(rename('core.min.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('concat.css', function () {
+function concatCSS() {
     cssLibs.push('dist/core.css');
 
     return gulp.src(cssLibs)
         .pipe(concat('lemurro.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('concat.min.css', function () {
+function concatMinCSS() {
     cssLibsMin.push('dist/core.min.css');
 
     return gulp.src(cssLibsMin)
         .pipe(concat('lemurro.min.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
 // JS
 
-gulp.task('core.js', function () {
+function coreJS() {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('core.js'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('core.min.js', function () {
+function coreMinJS() {
     return gulp.src('dist/core.js')
         .pipe(uglify())
         .pipe(rename('core.min.js'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('concat.js', function () {
+function concatJS() {
     jsLibs.push('dist/core.js');
 
     return gulp.src(jsLibs)
         .pipe(concat('lemurro.js'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('concat.min.js', function () {
+function concatMinJS() {
     jsLibsMin.push('dist/core.min.js');
 
     return gulp.src(jsLibsMin)
         .pipe(concat('lemurro.min.js'))
         .pipe(gulp.dest('dist'));
-});
+}
+
+// TASKS
+
+gulp.task('build', gulp.series(
+    gulp.parallel(
+        gulp.series(coreCSS, coreMinCSS, concatCSS, concatMinCSS),
+        gulp.series(coreJS, coreMinJS, concatJS, concatMinJS)
+    ), clean
+));
+
+gulp.task('watcher', gulp.series('build', gulp.parallel(watcherCSS, watcherJS)));
