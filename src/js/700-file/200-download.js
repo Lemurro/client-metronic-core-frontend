242a4b9a69889e67497dbc0af6496addf3ab0360
Дмитрий Щербаков <atomcms@ya.ru>
2019-05-15 14:33:51 +0500

    fix В методе на скачивание файла появился утерянный параметр `filename`

--- src/js/700-file/200-download.js
+++ src/js/700-file/200-download.js
@@ -1,14 +1,16 @@
 /**
  * Скачивание файла
  *
- * @param {integer} fileid ИД файла
+ * @param {integer|string} fileid   ИД постоянного файла или имя временного файла
+ * @param {string}         filename ИД файла
  *
- * @version 21.02.2019
+ * @version 15.05.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
-lemurro.file.download = function (fileid) {
+lemurro.file.download = function (fileid, filename) {
     lemurro.lightajax.post(true, pathServerAPI + 'file/download/prepare', {
-        fileid: fileid
+        fileid: fileid,
+        filename: filename
     }, function (result) {
         lemurro.lightajax.preloader('hide');
 
