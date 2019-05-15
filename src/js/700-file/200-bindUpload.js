447db6e3e7bac13837b8f5981f837f3615eb07b6
Дмитрий Щербаков <atomcms@ya.ru>
2019-05-15 11:44:10 +0500

    feat File: Добавлена новая функция onChange при навешивании загрузчика файлов на кнопку

--- src/js/700-file/200-bindUpload.js
+++ src/js/700-file/200-bindUpload.js
@@ -1,18 +1,33 @@
 /**
  * Подключение загрузчика файлов
  *
- * @param {jQuery}   btn      jQuery-объект указывающий на кнопку к которой привязать загрузчик
- * @param {function} callback Функция, вызываемая после успешной загрузки файла
+ * @param {jQuery}   btn        jQuery-объект указывающий на кнопку к которой привязать загрузчик
+ * @param {function} onComplete Функция, вызываемая после успешной загрузки файла
+ * @param {function} onChange   Функция, вызываемая сразу после выбора файла
  *
  * @version 15.05.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
-lemurro.file.bindUpload = function (btn, callback) {
+lemurro.file.bindUpload = function (btn, onComplete, onChange) {
     var btnContent = btn.html();
 
     new ss.SimpleUpload({
         button    : btn,
         url       : pathServerAPI + 'file/upload',
+        autoSubmit: false,
+        onChange  : function () {
+            var obj = this;
+
+            if (typeof onChange === 'function') {
+                onChange(function (result) {
+                    if (result === true) {
+                        obj.submit();
+                    }
+                });
+            } else {
+                obj.submit();
+            }
+        },
         onSubmit  : function () {
             btn.html('<i class="fas fa-spinner fa-pulse"></i> Загрузка...').prop('disabled', true); // change button text to "Uploading..."
         },
@@ -28,8 +43,8 @@ lemurro.file.bindUpload = function (btn, callback) {
             if (result.hasOwnProperty('errors')) {
                 lemurro.showErrors(result.errors);
             } else {
-                if (!isEmpty(callback)) {
-                    callback(result.data.id, 'add', filename, btn);
+                if (typeof onComplete === 'function') {
+                    onComplete(result.data.id, 'add', filename, btn);
                 }
 
                 swal('Выполнено', 'Файл <strong>' + filename + '</strong>' + ' успешно загружен', 'success');
