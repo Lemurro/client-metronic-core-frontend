0e33caab128e9bcaa7ae7e13329a398e01730a82
Дмитрий Щербаков <atomcms@ya.ru>
2020-05-11 14:07:53 +0500

    fix [Пользователи] В методе "lemurro.users.remove" необходимо удалить неиспользуемый параметр "name"

--- src/js/600-users/200-remove.js
+++ src/js/600-users/200-remove.js
@@ -2,13 +2,13 @@
  * Удаление
  *
  * @param {string}   id       ИД пользователя
- * @param {string}   name     Имя пользователя
  * @param {function} callback Функция обратного вызова
  *
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
- * @version 21.01.2020
+ *
+ * @version 11.05.2020
  */
-lemurro.users.remove = function (id, name, callback) {
+lemurro.users.remove = function (id, callback) {
     if (id === '1') {
         swal('Внимание!', 'Пользователь с id=1 не может быть удалён', 'warning');
     } else {
