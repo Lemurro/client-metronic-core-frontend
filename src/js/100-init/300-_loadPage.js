578aed2e028b45126fa2c31faa71c7cd17cccb1e
Дмитрий Щербаков <atomcms@ya.ru>
2020-08-15 20:58:57 +0500

    feat Переход на Metronic 7.0+

--- src/js/100-init/300-_loadPage.js
+++ src/js/100-init/300-_loadPage.js
@@ -1,8 +1,9 @@
 /**
  * Определим загруженную страницу и запустим ее init() если он есть
  *
- * @version 24.04.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 14.08.2020
  */
 lemurro._loadPage = function () {
     var pageFile = location.protocol + '//' + location.host + '/' + app.page.name + '.html';
@@ -10,19 +11,23 @@ lemurro._loadPage = function () {
     document.title = app.page.title + ' | ' + app.config.title;
 
     // Сбросим активность со всех ссылок
-    $('header').find('.js-link').each(function () {
-        var link = $(this);
+    $('#kt_header')
+        .find('.js-link')
+        .each(function () {
+            var link = $(this);
 
-        link.removeClass(link.attr('data-active-class'));
-    });
+            link.removeClass(link.attr('data-active-class'));
+        });
 
     $('#js-page').load(pageFile, function () {
         // Установим активность ссылок с определенным классом
-        $('header').find('.js-link__' + app.page.name).each(function () {
-            var link = $(this);
+        $('#kt_header')
+            .find('.js-link__' + app.page.name)
+            .each(function () {
+                var link = $(this);
 
-            link.addClass(link.attr('data-active-class'));
-        });
+                link.addClass(link.attr('data-active-class'));
+            });
 
         lemurro._bindForAll();
 
