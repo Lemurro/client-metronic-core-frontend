a04f6155584da72ef83fe037525d07ae33ac423d
Дмитрий Щербаков <atomcms@ya.ru>
2020-08-29 13:26:33 +0500

    build Релиз версии 1.9.2

--- dist/lemurro.js
+++ dist/lemurro.js
@@ -8061,15 +8061,16 @@ lemurro.tabs = {};
  *
  * @param {string} tabID Идентификатор нужного таба
  *
- * @version 26.10.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 29.08.2020
  */
 lemurro.tabs.showTab = function (tabID) {
     var tabsLinks    = $('#js-tabs__links');
     var tabsContents = $('#js-tabs__contents');
 
     tabsLinks.find('.nav-link').removeClass('active show');
-    tabsContents.find('.tab-pane').removeClass('active show');
+    tabsContents.children('.tab-pane').removeClass('active show');
 
     tabsLinks.find('a[data-target="#' + tabID + '"]').addClass('active show');
     tabsContents.find('#' + tabID).addClass('active show');
