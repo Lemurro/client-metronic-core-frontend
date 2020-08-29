613b8d5f583436e4c8f63d6cab1eed58a69d0716
Дмитрий Щербаков <atomcms@ya.ru>
2020-08-29 13:22:53 +0500

    fix [Tabs] Исправлена ошибка с показом табов если есть вложенные табы

--- src/js/400-tabs/1-showTab.js
+++ src/js/400-tabs/1-showTab.js
@@ -11,7 +11,7 @@ lemurro.tabs.showTab = function (tabID) {
     var tabsLinks    = $('#js-tabs__links');
     var tabsContents = $('#js-tabs__contents');
 
-    tabsLinks.children('.nav-link').removeClass('active show');
+    tabsLinks.find('.nav-link').removeClass('active show');
     tabsContents.children('.tab-pane').removeClass('active show');
 
     tabsLinks.find('a[data-target="#' + tabID + '"]').addClass('active show');
