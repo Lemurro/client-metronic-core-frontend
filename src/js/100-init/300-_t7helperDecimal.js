be1560b58a6b8d9f0ff01ced21d916426e8ca9f5
Дмитрий Щербаков <atomcms@ya.ru>
2020-11-05 14:03:52 +0500

    feat [Template7 Helper] Хелпер lemurrodecimal выполняет перед выводом value.toFixed(precision)

--- src/js/100-init/300-_t7helperDecimal.js
+++ src/js/100-init/300-_t7helperDecimal.js
@@ -1,11 +1,14 @@
 /**
  * Хелпер для Template7: lemurrodecimal (lemurro.helper.decimal)
  *
- * @version 12.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
+ *
+ * @version 05.11.2020
  */
 lemurro._t7helperDecimal = function () {
     Template7.registerHelper('lemurrodecimal', function (value, precision) {
-        return lemurro.helper.decimal(value, precision);
+        var result = lemurro.helper.decimal(value, precision);
+
+        return result.toFixed(2);
     });
 };
