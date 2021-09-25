944dc71e3ee83189c4e22dd9d4e99162712bf884
DimNS <atomcms@ya.ru>
2021-09-25 12:12:39 +0500

    feat [Version] Добавлено указание версий в api и web, когда они отлчичаются показывается сообщение о необходимости обновления окна браузера

--- src/js/200-auth/100-_success.js
+++ src/js/200-auth/100-_success.js
@@ -1,15 +1,38 @@
 /**
  * Проверка сессии и загрузка данных о пользователе прошли успешно
- *
- * @version 10.06.2019
- * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 lemurro.auth._success = function () {
-    // Установим идентификатор пользователя в верхнем меню
-    $('.js-user__auth-id').text(lemurro.userinfo.auth_id);
-
     // Приложение успешно стартовало
     app.started = true;
+
+    // Установим идентификатор пользователя в верхнем меню
+    $('header').find('.js-user__auth-id')
+        .text(lemurro.userinfo.first_name + ' ' + lemurro.userinfo.last_name);
+
+    console.log('appVersionFront', app.version);
+    console.log('appVersionBack', lemurro.userinfo['last_app_version']);
+
+    if (lemurro.userinfo['last_app_version'] !== '0' && lemurro.userinfo['last_app_version'] !== app.version) {
+        toastr.options = {
+            closeButton: true,
+            debug: false,
+            newestOnTop: false,
+            progressBar: false,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+            onclick: null,
+            showDuration: '1000',
+            hideDuration: '1000',
+            timeOut: '0',
+            extendedTimeOut: '0',
+            showEasing: 'swing',
+            hideEasing: 'linear',
+            showMethod: 'fadeIn',
+            hideMethod: 'fadeOut',
+        };
+
+        toastr.error('Пожалуйста, перезагрузите страницу для применения обновлений', 'Обновление');
+    }
 
     // Скрываем более ненужный оверлей прелоадера
     $('#js-preloader-overlay').hide();
