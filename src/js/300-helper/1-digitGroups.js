06e0b63a960fda6fb76997c23c87c55e1c063c3f
DimNS <atomcms@ya.ru>
2022-06-15 08:23:52 +0500

    feat [Helper] Добавлен новый хелпер digitGroups

---
+++ src/js/300-helper/1-digitGroups.js
@@ -0,0 +1,33 @@
+/**
+ * Группировка цифр по разрядам
+ *
+ * @param {string|number} value Строка или число
+ * @param {number} precision Точность результата (по умолчанию: 2)
+ *
+ * @return {number}
+ */
+lemurro.helper.digitGroups = function (value, precision) {
+    if (typeof value === 'string') {
+        value = parseFloat(value);
+    }
+
+    if (isEmpty(value)) {
+        return 0;
+    }
+
+    if (typeof value !== 'number') {
+        return 0;
+    }
+
+    if (typeof precision !== 'number' || isNaN(precision)) {
+        precision = 2;
+    }
+
+    var re = /(\d)(?=(\d\d\d)+([^\d]|$))/g;
+
+    if (precision === -1) {
+        return String(value).replace(re, '$1 ');
+    }
+
+    return value.toFixed(precision).replace(re, '$1 ');
+};
